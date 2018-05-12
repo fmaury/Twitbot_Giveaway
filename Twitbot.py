2f064388a37b0b5cbaf283c0c20d466617b3d90c
fmaury <ololzfanfan@hotmail.fr>
2018-05-13 00:23:44 +0200

    Config file added and follow bug fixed

--- Twitbot.py
+++ Twitbot.py
@@ -4,7 +4,9 @@ import yaml
 import sys
 import random
 import time
+import datetime
 import os
+import pprint
 
 
 def usage() :
@@ -12,11 +14,11 @@ def usage() :
     sys.exit(0)
 
 def getapi():
-    config = yaml.load(open("./token.yaml", 'r'))
-    consumer_key = config["CONSUMER_KEY"]
-    consumer_secret = config["CONSUMER_SECRET"]
-    access_token = config["ACCESS_TOKEN"]
-    access_secret = config["ACCESS_SECRET"]
+    token = yaml.load(open("./token.yaml", 'r'))
+    consumer_key = token["CONSUMER_KEY"]
+    consumer_secret = token["CONSUMER_SECRET"]
+    access_token = token["ACCESS_TOKEN"]
+    access_secret = token["ACCESS_SECRET"]
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
     return(tweepy.API(auth))
@@ -32,51 +34,78 @@ def follow(name) :
 
 def printTweetInfos(status) :
     print(status._json['user']['screen_name'])
-    #print (status.full_text)
     print (status.retweet_count)
+    if status._json['retweeted_status'] : 
+        print(status._json['retweeted_status']['created_at'])
+    else : 
+        print(status._json['created_at'])
+    #print (status)
 
-def taff(searchRequest) :
+def tooOld(date):
+    mois = {"January":1, "Febuary": 2}
+    print (date)
+    d = datetime.date.today()
+    print ("mois: " + str(d.month))
+    return (0)
+
+def taff(searchRequest, hashtag) :
     list_names = list()
     for status in searchRequest:
+        if tooOld(status._json['created_at']):
+            continue
+
         uid = status.id
         nbRT = int(status.retweet_count);
-        if "retweeted_status" in dir(status):
-            tweet=status.retweeted_status.full_text
+        if hasattr(status, 'retweeted_status'):
+            try:
+                tweet = status.retweeted_status.extended_tweet["full_text"]
+            except:
+                tweet = status.retweeted_status.full_text
         else:
-            tweet=status.full_text
+            try:
+                tweet = status.extended_tweet["full_text"]
+            except :
+                tweet = status.full_text
+        print (tweet.encode('utf-8'))
         texte = tweet.split(' ')
         printTweetInfos(status)
-        if (nbRT > 5 and hashtag.lower() != "concours" and hashtag.lower() != "#concours") \
-                or (nbRT > 1000 and (hashtag.lower() == "concours" or hashtag.lower() == "#concours")) :
-            try :
-                api.create_favorite(uid)
-            except Exception:
-                print ("Already liked !")
-            try :
-                api.retweet(uid)
-            except Exception:
-                print ("Already RT !")
-        if nbRT > 1000 :
-            follow(status._json['user']['screen_name']) 
-            for names in texte:
-                if names and names[0] and names[0] == '@':
-                    follow(names[1:])
-        del list_names[:]
-        print("################################################################")
-        ####### ACTIVER APRES PHASE TEST #######
-        time.sleep(random.randrange(2, 10, 1))
+       # if hashtag == "42genesys" or (nbRT > config['nbRtLikeRt'] and hashtag.lower() != "concours" and hashtag.lower() != "#concours") \
+       #         or (nbRT > config['nbRtFollow'] and (hashtag.lower() == "concours" or hashtag.lower() == "#concours")) :
+       #     try :
+       #         api.create_favorite(uid)
+       #     except Exception:
+       #         print ("Already liked !")
+       #     try :
+       #         api.retweet(uid)
+       #     except Exception:
+       #         print ("Already RT !")
+       # if nbRT > config['nbRtFollow'] :
+       #     follow(status._json['user']['screen_name']) 
+       #     for names in texte:
+       #         if names and names[0] and names[0] == '@':
+       #             if names[len(names) - 1] != '.' and names[len(names) - 1] != ',':
+       #                 follow(names[1:])
+       #             else :
+       #                 follow(names[1:-1])
+       # del list_names[:]
+       # print("################################################################")
+       # ####### ACTIVER APRES PHASE TEST #######
+       # time.sleep(random.randrange(2, 10, 1))
+       # time.sleep(1)
 
 if __name__ == "__main__":
+    config = yaml.load(open("./config.yaml", 'r'))
     api = getapi()
-    my_infos = api.me()
-    my_id = my_infos._json['id']
     hashtag = sys.argv[1]
     if  len(sys.argv) < 2 or (len(sys.argv) < 3 and hashtag != 'followback') :
         usage()
     if hashtag == 'followback' :
+        my_infos = api.me()
+        my_id = my_infos._json['id']
         for follower in api.followers(my_id):
             follow(follower._json['screen_name'])
             print(follower._json['screen_name'])
+        sys.exit(0)
     else :
         if hashtag == 'trend' :
             print ("Trend: " + api.trends_place(23424819)[0]['trends'][0]['query'])
@@ -84,4 +113,4 @@ if __name__ == "__main__":
     if sys.argv[2].isdigit() == False :
         usage()
     searchRequest = tweepy.Cursor(api.search, q=hashtag, lang='fr', tweet_mode="extended").items(int(sys.argv[2]))
-    taff(searchRequest)   
+    taff(searchRequest, hashtag)   
